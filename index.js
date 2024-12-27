e9d2224fbdc9c14e5761243b60c1fb7d4e3b44d1
JCDWHIZ <jesseugboh@gmail.com>
2024-12-27 13:07:57 +0100

    added api documentation

--- index.js
+++ index.js
@@ -3,12 +3,32 @@ const dotenv = require("dotenv");
 const routes = require("./routes");
 const cors = require("cors");
 const { connectToDb } = require("./config/db");
+const axios = require("axios");
 
 const app = express();
 
 dotenv.config();
 
 connectToDb();
+
+app.get("/", async (req, res) => {
+  try {
+    res.send(`
+    <!DOCTYPE html>
+    <html>
+    <head>
+      <title>Welcome</title>
+    </head>
+    <body>
+      <h1>Welcome</h1>
+      <p>To view the documentation for this api go <a href="https://www.apidog.com/apidoc/shared-2d84819f-6dc6-4ea3-82ac-e369d8d9a4c0">here</a></p>
+    </body>
+    </html>
+  `);
+  } catch (error) {
+    res.status(500).send("An error occured");
+  }
+});
 
 app.use(express.json());
 app.use(cors());
